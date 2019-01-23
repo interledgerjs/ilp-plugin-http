cb467deebddfc8b1cf009274768bf9d5d164a5ef
sharafian <ben@sharafian.me>
2019-01-23 14:13:45 -0800

    feat: implement server-side

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fetch = require('node-fetch')
 const jwt = require('jsonwebtoken')
 // TODO: module for http2
 
+const MAX_ILP_PACKET_LENGTH = 32767
+
 class PluginHttp {
   constructor ({ incoming = {}, outgoing = {} } = {}) {
     // TODO: validate args
@@ -30,22 +32,57 @@ class PluginHttp {
   async connect () {
     if (this._connected) return
 
-    // TODO: start the server
+    this._app = new Koa()
+    this._app.use(async (ctx, next) => {
+      const verified = await this._verifyToken(ctx.headers.authorization)
+      if (!verified) {
+        ctx.throw(401, 'invalid authorization')
+        return
+      }
+
+      if (!this._connected) {
+        ctx.throw(502, 'server is closed')
+        return
+      }
+
+      const packet = raw(ctx.req, {
+        limit: MAX_ILP_PACKET_LENGTH 
+      })
+
+      if (!this._dataHandler) {
+        ctx.throw(502, 'no handler registered.')
+        return
+      }
+
+      ctx.body = await this._dataHandler(packet)
+    })
+
+    this._httpServer = this._app.listen(this._port)
+    this._connected = true
   }
 
   async disconnect () {
     if (!this._connected) return
 
-    // TODO: stop the server
+    this._connected = false
+    this._httpServer.close()
   }
 
-  async _getToken () {
+  _verifyToken (token) {
+    return new Promise(resolve => {
+      jwt.verify(token, this._incomingSecret, err => {
+        resolve(!err)
+      })
+    })
+  }
+
+  _getToken () {
     if (this._tokenSignedAt > Date.now() + this._tokenExpiry / 2) {
       return this._token
     }
 
     this._signedAt = Date.now()
-    const token = await new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       jwt.sign({}, this._outgoingSecret, {
         expiresIn: Math.floor(this._tokenExpiry / 1000)
       }, (err, token) => {
@@ -55,7 +92,7 @@ class PluginHttp {
     })
   }
 
-  sendData (data) {
+  async sendData (data) {
     if (!this._connected) {
       throw new Error('plugin is not connected.')
     }
@@ -67,7 +104,7 @@ class PluginHttp {
       method: 'POST',
       body: data,
       headers: {
-        Authorization: this._getToken()
+        Authorization: this._getToken(),
         'Content-Type': 'application/ilp+octet-stream'
       }
     })
