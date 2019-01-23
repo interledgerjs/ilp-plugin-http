6e4b77f2bac29d748336da7e8aad7311535e0865
sharafian <ben@sharafian.me>
2019-01-23 15:32:33 -0800

    feat: add multilateral mode

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Koa = require('koa')
+const IlpPacket = require('ilp-packet')
+const ILDCP = require('ilp-protocol-ildcp')
 const fetch = require('node-fetch')
 const EventEmitter = require('events')
 const jwt = require('jsonwebtoken')
@@ -6,13 +8,16 @@ const raw = require('raw-body')
 // TODO: module for http2
 
 const MAX_ILP_PACKET_LENGTH = 32767
+const INVALID_SEGMENT = new RegExp('[^A-Za-z0-9_\\-]')
 
 class PluginHttp extends EventEmitter {
-  constructor ({ incoming = {}, outgoing = {} } = {}) {
+  constructor ({ multi, ildcp, incoming = {}, outgoing = {} } = {}) {
     super()
 
     // TODO: validate args
     this._connected = false
+    this._multi = !!multi
+    this._ildcp = ildcp
 
     // incoming
     this._port = incoming.port
@@ -22,6 +27,7 @@ class PluginHttp extends EventEmitter {
     this._url = outgoing.url
     this._http2 = !!outgoing.http2
     this._outgoingSecret = outgoing.secret
+    this._name = outgoing.name || this._port
 
     this._token = null 
     this._tokenExpiry = outgoing.tokenExpiry || 30000
@@ -52,6 +58,20 @@ class PluginHttp extends EventEmitter {
       const packet = await raw(ctx.req, {
         limit: MAX_ILP_PACKET_LENGTH 
       })
+
+      if (this._multi) {
+        const parsed = IlpPacket.deserializeIlpPrepare(packet)
+        const name = ctx.get('ilp-peer-name')
+
+        if (parsed.destination === 'peer.config') {
+          const ildcp = await this._fetchIldcp()
+          ctx.body = ILDCP.serializeIldcpResponse({
+            ...ildcp,
+            clientAddress: ildcp.clientAddress + '.' + name
+          })
+          return
+        }
+      }
 
       if (!this._dataHandler) {
         ctx.throw(502, 'no handler registered.')
@@ -101,6 +121,29 @@ class PluginHttp extends EventEmitter {
     return this._token
   }
 
+  async _fetchIldcp () {
+    if (!this._ildcp) {
+      this._ildcp = await ILDCP.fetch(this._dataHandler)
+    }
+    return this._ildcp
+  }
+
+  // Only used in multilateral situation
+  async _generateUrl (data) {
+    const ildcp = await this._fetchIldcp()
+    const { destination } = IlpPacket.deserializeIlpPrepare(data)
+    const segment = destination
+      .substring(ildcp.clientAddress.length)
+      .split('.')[0]
+
+    if (INVALID_SEGMENT.test(segment)) {
+      throw new Error('invalid address segment')
+    }
+
+    // splice the address segment into the URL
+    return this._url.replace('%', segment)
+  }
+
   async sendData (data) {
     if (!this._connected) {
       throw new Error('plugin is not connected.')
@@ -109,12 +152,18 @@ class PluginHttp extends EventEmitter {
     // TODO: is it possible to authenticate a whole connection at
     // establishment? maybe using client certs?
 
-    const res = await fetch(this._url, {
+    // url may be templated in multilateral environment
+    const url = this._multi
+      ? this._generateUrl(data)
+      : this._url
+
+    const res = await fetch(url, {
       method: 'POST',
       body: data,
       headers: {
         Authorization: await this._getToken(),
-        'Content-Type': 'application/ilp+octet-stream'
+        'Content-Type': 'application/ilp+octet-stream',
+        'Ilp-Peer-Name': this._name
       }
     })
 
