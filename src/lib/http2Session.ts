eb92e30ad004358d9cf05250b0ad44a0924a4e23
sharafian <ben@sharafian.me>
2019-02-27 11:46:57 -0800

    feat: initial connection pooling logic

---
+++ src/lib/http2Session.ts
@@ -0,0 +1,94 @@
+import * as makeDebug from 'debug'
+import * as http2 from 'http2'
+
+const debug = makeDebug('http2session')
+
+enum ConnectState {
+  DISCONNECTED,
+  CONNECTING,
+  CONNECTED
+}
+
+export default class Http2Session {
+  private _authority: string
+  private _http2Opts: object
+  private _requests: number
+
+  private _connected: ConnectState
+  private _connectPromise: Promise<http2.ClientHttp2Session>
+  private _client: http2.ClientHttp2Session
+
+  constructor (authority, http2Opts) {
+    this._authority = authority
+    this._http2Opts = http2Opts
+    this._connected = ConnectState.DISCONNECTED
+    this._requests = 0
+  }
+
+  async allocateRequest (): Promise<http2.ClientHttp2Session | undefined> {
+    const client = await this.connect()
+    if (this._requests >= client.remoteSettings.maxConcurrentStreams) {
+      return
+    } else {
+      this._requests++
+      return client
+    }
+  }
+
+  freeRequest (): void {
+    this._requests--
+  }
+
+  _connect () {
+    if (this._connected === ConnectState.CONNECTED && this._client) {
+      return Promise.resolve(this._client)
+    } else if (this._connected === ConnectState.CONNECTING && this._connectPromise) {
+      return this._connectPromise
+    }
+
+    debug('creating client. authority=', this._authority)
+    const client = http2.connect(this._authority, this._http2Opts)
+
+    this._client = client
+    this._connected = ConnectState.CONNECTING
+    this._connectPromise = new Promise((resolve, reject) => {
+      const cleanUp = () => {
+        client.removeListener('connect', onConnect)
+        client.removeListener('error', onError)
+        client.removeListener('close', onClose)
+      }
+
+      const onConnect = () => {
+        debug('client connected.')
+        client.removeListener('connect', onConnect)
+        this._connected = ConnectState.CONNECTED
+        resolve(client)
+      }
+
+      const onClose = () => {
+        debug('client closed.')
+        cleanUp()
+        this._connected = ConnectState.DISCONNECTED
+        reject(new Error('closed while opening connection'))
+      }
+
+      // TODO: log the error
+      const onError = (error: Error) => {
+        debug('client encountered error. error=', error.message)
+        cleanUp()
+        this._connected = ConnectState.DISCONNECTED
+        reject(error)
+      }
+
+      client.on('connect', onConnect)
+      client.on('close', onClose)
+      client.on('error', onError)
+    })
+  }
+
+  close () {
+    if (this._client) {
+      this._client.close()
+    }
+  }
+}
