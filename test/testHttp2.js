bb95fbaab71926f083704d1f6c52ea8936edb0e0
sharafian <ben@sharafian.me>
2019-02-27 12:03:40 -0800

    test: fetch 5k times to replicate max streams error

--- test/testHttp2.js
+++ test/testHttp2.js
@@ -4,11 +4,14 @@ async function run () {
   console.log('making client')
   const client = new Http2Client('https://coil.com')
 
-  console.log('fetching site')
-  const result = await client.fetch('/', {})
+  console.log('fetching site 5k times')
+  const result = await Promise.all([ ...Array(5000).keys() ].map(() => {
+    return client.fetch('/', {})
+  }))
 
-  console.log(result)
-  console.log(result.buffer().toString())
+  console.log('fetched.')
+  console.log(result[0])
+  console.log(result[0].buffer().toString())
 }
 
 run()
