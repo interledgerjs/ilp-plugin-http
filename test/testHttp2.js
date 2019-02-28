3f4e25835e4ec6a0b70bfa06eb20e86a08f1588d
sharafian <ben@sharafian.me>
2019-02-28 10:17:57 -0800

    test: run 50k requests to get more errors

--- test/testHttp2.js
+++ test/testHttp2.js
@@ -2,14 +2,30 @@ const Http2Client = require('../build/lib/http2').default
 
 async function run () {
   console.log('making client')
-  const client = new Http2Client('https://coil.com')
+  const client = new Http2Client('https://staging.coil.com')
 
   console.log('fetching site 5k times')
-  const result = await Promise.all([ ...Array(5000).keys() ].map(() => {
-    return client.fetch('/', {})
-  }))
+  let result
+
+  let successes = 0
+  let failures = 0
+
+  for (let i = 0; i < 10; ++i) {
+    console.log('iteration', i)
+    result = await Promise.all([ ...Array(5000).keys() ].map(async () => {
+      try {
+        const result = await client.fetch('/', {})
+        successes++
+        return result
+      } catch (e) {
+        failures++
+      }
+    }))
+  }
 
   console.log('fetched.')
+  console.log('success:', successes)
+  console.log('failure:', failures)
   console.log(result[0])
   console.log(result[0].buffer().toString())
 }
