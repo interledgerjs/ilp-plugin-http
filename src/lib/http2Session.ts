dac959a7c47bfae6fbca38780141049dfc8547ed
sharafian <ben@sharafian.me>
2019-02-28 10:36:25 -0800

    fix: add limit on max requests per session

--- src/lib/http2Session.ts
+++ src/lib/http2Session.ts
@@ -3,6 +3,7 @@ import * as http2 from 'http2'
 
 const debug = makeDebug('http2session')
 const DEFAULT_MAX_STREAMS = (2 ** 31) - 1
+const MAX_TOTAL_REQUESTS = Infinity
 
 enum ConnectState {
   DISCONNECTED,
@@ -14,16 +15,23 @@ export default class Http2Session {
   private _authority: string
   private _http2Opts: object
   private _requests: number
+  private _totalRequests: number
+  private _maxRequests: number
 
   private _connected: ConnectState
   private _connectPromise: Promise<http2.ClientHttp2Session>
   private _client: http2.ClientHttp2Session
 
-  constructor (authority: string, http2Opts: object) {
+  public id: string
+
+  constructor (authority: string, http2Opts: object, maxRequests?: number) {
+    this.id = String(Math.random()).substring(2)
     this._authority = authority
     this._http2Opts = http2Opts
     this._connected = ConnectState.DISCONNECTED
     this._requests = 0
+    this._totalRequests = 0
+    this._maxRequests = maxRequests || MAX_TOTAL_REQUESTS
   }
 
   async allocateRequest (): Promise<http2.ClientHttp2Session | undefined> {
@@ -31,10 +39,11 @@ export default class Http2Session {
     const maxStreams = client.remoteSettings.maxConcurrentStreams ||
       DEFAULT_MAX_STREAMS
 
-    if (this._requests >= maxStreams) {
+    if (this._requests >= maxStreams || this._totalRequests >= this._maxRequests) {
       return
     } else {
       this._requests++
+      this._totalRequests++
       return client
     }
   }
@@ -44,7 +53,9 @@ export default class Http2Session {
   }
 
   _connect (): Promise<http2.ClientHttp2Session> {
-    if (this._connected === ConnectState.CONNECTED && this._client) {
+    if (this._requests === 0 && this._totalRequests >= this._maxRequests) {
+      debug('hit maximum requests; resetting connection')
+    } else if (this._connected === ConnectState.CONNECTED && this._client) {
       return Promise.resolve(this._client)
     } else if (this._connected === ConnectState.CONNECTING && this._connectPromise) {
       return this._connectPromise
@@ -53,6 +64,7 @@ export default class Http2Session {
     debug('creating client. authority=', this._authority)
     const client = http2.connect(this._authority, this._http2Opts)
 
+    this._totalRequests = 0
     this._client = client
     this._connected = ConnectState.CONNECTING
     this._connectPromise = new Promise((resolve, reject) => {
